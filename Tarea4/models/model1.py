ed8865d7e00ee6d71e371f5ccf21098b1e06df49
Gonza Rigi <95933720+gp-gv-nc@users.noreply.github.com>
2022-07-22 11:01:11 -0300

    edition

--- Tarea4/models/model1.py
+++ Tarea4/models/model1.py
@@ -1,9 +1,4 @@
-"""
-Model exported as python.
-Name : model1
-Group : 
-With QGIS : 32208
-"""
+# Se importan paquetes
 
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
